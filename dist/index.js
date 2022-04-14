89f180ac4e5de2a5136b2b5290fce7fa74db7f03
iNorthIsle <ndobox@outlook.com>
2022-04-14 17:30:49 +0800

    chore: update build

--- dist/index.js
+++ dist/index.js
@@ -1207,23 +1207,23 @@ const klawSync = __webpack_require__(502);
 const { lookup } = __webpack_require__(779);
 
 const AWS_KEY_ID = core.getInput('aws_access_key_id', {
-  required: true
+	required: true
 });
 
 const SECRET_ACCESS_KEY = core.getInput('aws_secret_access_key', {
-  required: true
+	required: true
 });
 
 const BUCKET = core.getInput('aws_s3_bucket_name', {
-  required: true
+	required: true
 });
 
 const SOURCE_DIR = core.getInput('source_dir', {
-  required: true
+	required: true
 });
 
 const DESTINATION_DIR = core.getInput('destination_dir', {
-  required: false
+	required: false
 });
 
 const AWS_REGION = core.getInput('aws_region', {
@@ -1231,56 +1231,59 @@ const AWS_REGION = core.getInput('aws_region', {
 });
 
 const s3 = new S3({
-  accessKeyId: AWS_KEY_ID,
-  secretAccessKey: SECRET_ACCESS_KEY,
+	accessKeyId: AWS_KEY_ID,
+	secretAccessKey: SECRET_ACCESS_KEY,
 	region: AWS_REGION
 });
 
 const destinationDir = DESTINATION_DIR === '/' ? shortid() : DESTINATION_DIR;
 
 const paths = klawSync(SOURCE_DIR, {
-  nodir: true
+	nodir: true
 });
 
 function upload(params) {
-  return new Promise(resolve => {
-    s3.upload(params, (err, data) => {
-      if (err) core.error(err);
-      core.info(`uploaded - ${data.Key}`);
-      core.info(`located - ${data.Location}`);
-      resolve(data.Location);
-    });
-  });
+	return new Promise(resolve => {
+		s3.upload(params, (err, data) => {
+			if (err) core.error(err);
+			core.info(`uploaded - ${data.Key}`);
+			core.info(`located - ${data.Location}`);
+			resolve(data.Location);
+		});
+	});
 }
 
 function run() {
-  const sourceDir = path.join(process.cwd(), SOURCE_DIR);
-  return Promise.all(
-    paths.map(p => {
-      const fileStream = fs.createReadStream(p.path);
-      const bucketPath = path.join(destinationDir, path.relative(sourceDir, p.path));
-      const params = {
-        Bucket: BUCKET,
-        Body: fileStream,
-        Key: bucketPath,
-        ContentType: lookup(p.path) || 'text/plain'
-      };
-      return upload(params);
-    })
-  );
+	const sourceDir = path.join(process.cwd(), SOURCE_DIR);
+	const regex = /^.*\.(dmg|zip|exe|AppImage|yml)$/
+	const validPaths = paths.filter(p => new RegExp(regex).test(p.path));
+
+	return Promise.all(
+		validPaths.map(p => {
+			const fileStream = fs.createReadStream(p.path);
+			const bucketPath = path.join(destinationDir, path.relative(sourceDir, p.path));
+			const params = {
+				Bucket: BUCKET,
+				Body: fileStream,
+				Key: bucketPath,
+				ContentType: lookup(p.path) || 'text/plain'
+			};
+			return upload(params);
+		})
+	);
 }
 
 run()
-  .then(locations => {
-    core.info(`object key - ${destinationDir}`);
-    core.info(`object locations - ${locations}`);
-    core.setOutput('object_key', destinationDir);
-    core.setOutput('object_locations', locations);
-  })
-  .catch(err => {
-    core.error(err);
-    core.setFailed(err.message);
-  });
+	.then(locations => {
+		core.info(`object key - ${destinationDir}`);
+		core.info(`object locations - ${locations}`);
+		core.setOutput('object_key', destinationDir);
+		core.setOutput('object_locations', locations);
+	})
+	.catch(err => {
+		core.error(err);
+		core.setFailed(err.message);
+	});
 
 
 /***/ }),
